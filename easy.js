0cd8bfc73f4dbc2440f36ced36f7213f3df6ca2f
KeithChou <keithchou95@gmail.com>
2017-08-13 14:13:35 +0800

    增加childrens, parents方法

--- easy.js
+++ easy.js
@@ -16,6 +16,47 @@ class Easy {
       cb(e)
     }, async)
   }
+  childrens (target, child) {
+    if (!(target instanceof Object)) {
+      target = this.one(target)
+    }
+    if (!(child instanceof Object)) {
+      child = this.one(child)
+    }
+    let itChildList = target.children
+    for (let i = 0; i < itChildList.length; i++) {
+      let node = itChildList[i]
+      if (node === child) {
+        return child
+      }
+      if (node.hasChildNodes()) {
+        node = this.childrens(node, child)
+        if (node !== null) {
+          return node
+        }
+      }
+    }
+    return null
+  }
+  parents (target, parent) {
+    if (!(target instanceof Object)) {
+      target = this.one(target)
+    }
+    if (!(parent instanceof Object)) {
+      parent = this.one(parent)
+    }
+    if (parent == null || target == null) {
+      return null
+    }
+    if (target === parent) {
+      return target
+    }
+    let itParent = target.parentNode
+    while (itParent !== parent) {
+      itParent = itParent.parentNode
+    }
+    return itParent
+  }
 }
 
 const $ = new Easy()
