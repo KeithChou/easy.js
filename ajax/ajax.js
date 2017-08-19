1e7a8568f1337c66339139cc73a00606f12c0735
KeithChou <keithchou95@gmail.com>
2017-08-19 21:08:41 +0800

    调整ajax模块

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -5,25 +5,70 @@ class Ajax {
       type: 'get',
       dataType: 'json',
       data: {},
-      timeout: 10000,
+      async: true,
+      time: 10000,
       success (data) {},
-      error (err) {}
+      fail (err) {}
+      timeout (err) {},
+      error () {}
     }
     this.options = Object.assign({}, setting, options)
   }
   init (() {
     this.createRequest()
   })()
+  handleResponse (data) {
+    if (data) {
+      if (data.success) {
+        this.options.success(data.data)
+      } else {
+        if (this.options.fail instanceof Function) {
+          this.options.fail(data.data)
+        }
+      }
+    } else {
+      this.options.error()
+    }
+  }
+  handleTimeOut () {
+    window.setTimeout(timer => {
+      if (this.options.timeout instanceof Function) {
+        this.options.timeout('timeout')
+        return
+      }
+    }, this.options.time)
+  }
   createRequest() {
-    let xhr = new window.XMLHttpRequest()
+    let [xhr, type, url, async] = [new window.XMLHttpRequest(), this.options.type.toLowerCase(), this.options.url, this.options.async]
+    let data = this.paramToString(this.options.data)
     xhr.addEventListener('readystatechange', () => {
       if (xhr.readyState === 4) {
         if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
           let data = JSON.parse(xhr.responsetText)
-          this.options.success(data)
+          this.handleResponse(data)
+        } else {
+          this.handleResponse()
         }
       }
     })
+    if (type === 'get') {
+      xhr.open(type, `${url}?${data}`, async)
+      xhr.send(null)
+    } else {
+      xhr.open(type, url, async)
+      xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencode')
+      xhr.send(data)
+    }
+    this.handleTimeOut()
+  }
+  paramToString (obj) {
+    let [str = ''] = []
+    for (let i in obj) {
+      let name = window.encodeURIComponent(i)
+      let value = window.encodeURIComponent(obj[i])
+      str +=  `&${name}=${value}`
+    }
+    return str.slice(1)
   }
 }
 
--- easy.js
+++ easy.js
@@ -57,6 +57,46 @@ class Easy {
     }
     return itParent
   }
+  error (text) {
+    let err = new Error(text)
+    throw err
+  }
+  on (event, target, cb, bubble = false) {
+    let doc = document
+    doc.addEventListener(event, e => {
+      let obj = e.target
+      let { id, className, name } = {
+        id: obj.id,
+        className: obj.className,
+        name: obj.name
+      }
+      if (id) {
+        switch (id) {
+          case `${target.slice(1)}`:
+            cb(e)
+            break
+          default:
+            this.error('未找到id对应的DOM节点，document事件代理失败')
+        }
+      } else if (className) {
+        switch (className) {
+          case `${target.slice(1)}`:
+            cb(e)
+            break
+          default:
+            this.error('未找到class对应的DOM节点，document事件代理失败')
+        }
+      } else if (name) {
+        switch (name) {
+          case target:
+            cb(e)
+            break
+          default:
+            this.error('未找到name对应的DOM节点，document事件代理失败')
+        }
+      }
+    }, bubble)
+  }
 }
 
 const $ = new Easy()
