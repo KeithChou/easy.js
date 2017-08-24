a2c375c15001f4f4ee7e9ba4f381c6e6da43bf9b
KeithChou <keithchou95@gmail.com>
2017-08-24 18:37:57 +0800

    增加format时间格式化函数

--- easy.js
+++ easy.js
@@ -61,6 +61,28 @@ class Easy {
     let err = new Error(text)
     throw err
   }
+  // @params (时间戳, 'YYYY-MM-DD hh:mm:ss'(默认值))
+  // @return 2017-8-16 22:17:53
+  formatDate (unix, format) {
+    let date = new Date(UNIX)
+    let T = {
+      'Y': date.getFullYear(),
+      'M': date.getMonth() + 1,
+      'D': date.getDate(),
+      'h': date.getHours(),
+      'm': date.getMinutes(),
+      's': date.getSeconds()
+    }
+    for (let i of Object.keys(T)) {
+      let reg = new RegExp(`(${i})+`)
+      format = format.replace(reg, i)
+    }
+    format = format.replace(/[YMDhms]/g, (val, index, arr) => {
+      val = T[val]
+      return val
+    })
+    return format
+  }
   on (event, target, cb, bubble = false) {
     let doc = document
     doc.addEventListener(event, e => {
